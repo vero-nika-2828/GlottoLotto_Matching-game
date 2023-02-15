cd0f9e435fe31d9715fc9b796477e6b274d4416e
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-02-15 21:33:58 +0000

    Add rotation  to  front-card styling in style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 let cards = document.getElementsByClassName("card");
-let cardBack = document.getElementsByClassName("card-back")
+
 
 for (let card of cards){
     card.addEventListener("click", function(){
-        card.classList.add("changecard");
-        if(this.getAttribute("class") === "card-front"){
-            cardBack.classList.add(visibility);
-        }    
+        card.classList.add("turn-card");
+
     })
 };
