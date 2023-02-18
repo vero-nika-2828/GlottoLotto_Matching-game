398ab86c24b3e9cba07ab16ae004da597970d5a5
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-02-18 01:54:11 +0000

    Update turnCard function in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,32 @@
 let cards = document.getElementsByClassName("card");
 const clickedCards = []
+let cardAmount = clickedCards.length
 
 //On a click, a card turns around and is marked as turned
 
 for (let card of cards){
-    card.addEventListener("click", function(){
-        card.classList.add("turn-card");
-        let individualCard = this.setAttribute("type", "clickedCard");
-        clickedCards.push(individualCard);
-        
-    })
+    card.addEventListener("click", cardTurns);
 };
+
+
+function cardTurns(){
+    this.classList.add("turn-card");
+    let individualCard = this.setAttribute("type", "clickedCard");
+    clickedCards.push(individualCard); 
+}
 
 //If no card was previously clicked and mark clicked otherwise compare clicked card to the currently clicked card 
 //data type could be clicked 
 //maybe need to add clickedCards.lenght
 
-if (clickedCards.length === 2){
-    
-    if (clickedCards[0] === clickedCards[1]){
-        matchFound();
-    }
-} else{
-    noMatch()
-};
+//if (clickedCards.length === 2){
+//    cards.removeEventListener("click", cardTurns);
+//};
 
-matchFound(alert("YOu have got a match"))
+if (parseInt(cardAmount) === parseInt(2)){
+    alert("OK")
+    cards.removeEventListener("click", cardTurns);
+}
 
 //When we have two cards in an array - get (1)the alt from the child of a child of card  (card - card-front - img - alt)
 //or maybe get this. get element alt 
