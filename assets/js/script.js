d2883322204cd6665560fc4abb2848ee938c9952
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-03-06 20:53:11 +0000

    Add compareCards function in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,55 +2,75 @@ let cards = document.getElementsByClassName("card");
 let clickedCards = [];
 let cardAmount = clickedCards.length;
 let counter = 0;
-let cardOne = (JSON.stringify$(".active")[0]);
-let imageArray = []
+let cardOne = $(".active")[0];
+let cardArray = [];
+let matchedCards = [];
+
+
 //On each click, class attribute is added which prompts the clicked card to turn
 //Clicked card is added to an array
 
 $(".card").on("click", cardTurns);
 
-function cardTurns(){
-    counter++;
-    $(this).addClass("active");
-    $(this).addClass("turn-card");
-    let individualCard= document.getElementsByClassName("active")
-    clickedCards.push(counter);  
-    console.log(JSON.stringify(clickedCards)); 
-    twoCardsSelected();
+function cardTurns(){  
+    if(!$(this).hasClass("active")){
+        counter++;
+        $(this).addClass("active");
+        $(this).addClass("turn-card");
+        //let cardText = $(this)[0].textContent;
+        let individualCard = $(this)[0].dataset.fruit;
+        cardArray.push(individualCard);
+        twoCardsSelected();   
+        let cardText = $(this)[0].textContent;
+        console.log(cardText);
+           
+    
+    }else{
+        console.log("it is alraedy active");
+        $(".active").off("click");
       
-}
+}}
 
+   
+ 
+//On each click, class attribute is added which prompts the clicked card to turn
+//Clicked card is added to an array
 
 
 function twoCardsSelected(){
     if(counter === 2){
-        let turnsIntitial = document.getElementById("turns").innerHTML += 1;
-        turnsIntitial++;
+        document.getElementById("turns").innerHTML ++;
         console.log("Two cards selected");
-        $(".card").off("click", cardTurns);
-        compareCards()         
+        $(".card").off("click", cardTurns); //instead of taking this off could I maybe delay the funtion of clicking again and put it after compare function
+        setTimeout(compareCards,1000);
+       
     }
 }
 
-function compareCards(card1, card2) {
-    console.log(cardOne)
-     
-}
+function compareCards() {
+    let card1 = cardArray[0];
+    let card2 = cardArray[1];
+    counter = 0;
+    cardArray =[];
 
-function compare(){
-    if(cards.getElementsByClassName === "active"){
-        //pull the ID text and compare 
-        let card1= this.cardId.text
-        let card2=
-        card1 =card2
-    }
-}
+    if(card1 === card2){
+        console.log("you have a match");   
+        $(".active").addClass("matched");
+        $(".matched").children().fadeOut();
+             
+    
+        let currentMatch = $(".matched");
+        matchedCards.push(currentMatch);
+        $(".card").on("click", cardTurns);
 
-//When two cards get added to an array other cards cannot be flipped and Turns will increase
-//if (parseInt(cardAmount) === parseInt(2)) {
-//    alert("works");
-//    cards.removeEventListener("click", cardTurns);
-//}
+
+
+
+    } else {
+        console.log("Try again");
+        $(".active").removeClass("active turn-card");
+        $(".card").on("click", cardTurns);
+    }};
 
 //Game countdown
 
@@ -64,7 +84,14 @@ function timeDecrease() {
     let secs = timeInSeconds % 60 ;
     
     timeInSeconds--;
-    document.getElementById("countdown").innerHTML = mins + ":" +secs;
+   
+
+    if(secs < 10){
+        document.getElementById("countdown").innerHTML = mins + ":" + "0" + secs;
+    } else {
+        document.getElementById("countdown").innerHTML = mins + ":" +secs;
+    }
+
 
     if(mins & secs < 0){
         console.log("Time out")
@@ -74,10 +101,10 @@ function timeDecrease() {
 }
 
 let gameSong = new Audio();
-gameSong.src = "./assets"  
+gameSong.src = "./assets/audio/mixkit-cartoon-toy-whistle-616.wav"  
 
 gameSong.addEventListener("click", playMusic);
 
 function playMusic(){
-
+       gameSong.play();
 }
